a6a4462e9670638ebe73e9bef0798cd229a8a792
James-Ashley <jkbashley@gmail.com>
2020-11-09 13:50:59 -0500

    adjusting files

--- WeatherPy/config.py
+++ WeatherPy/config.py
@@ -1,5 +1,5 @@
 # OpenWeatherMap API Key
-weather_api_key = "26cb1df89674bc8a76e27a3e5c4ca638"
+weather_api_key = "b1b5be0ba9352f1e1ed5dc6ae84cee3a"
 
 # Google API Key
 g_key = "AIzaSyD9mKL75yzdzK5ghTRfoAQUL1GJH5xpj9o"
